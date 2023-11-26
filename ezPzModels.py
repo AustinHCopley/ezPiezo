bb53cee215c1f8a741c85192542ca45ec1168564
Austin Copley <90229145+AustinHCopley@users.noreply.github.com>
2023-11-25 19:10:28 -0500

    added models, datasets, and visualization

---
+++ ezPzModels.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+class ezPzLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(ezPzLSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+    
+
+class ezPzCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(ezPzCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.fc1 = nn.Linear(32 * 120 * 160, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = self.pool(x)
+        x = torch.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
---
+++ pzData.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+import cv2
+
+class pzNpz(Dataset):
+    """Custom dataset for my npz files"""
+    def __init__(self, label_file, data_dir, transform=None):
+        self.labels = pd.read_csv(label_file)
+        self.data_dir = data_dir
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        filename = self.labels.iloc[idx]['Filename']
+        npz = np.load(os.path.join(self.data_dir, filename))
+        data = np.asarray([npz["arr_0"], npz["arr_1"]])
+
+        if self.transform:
+            data = self.transform(data)
+        
+        return np.asarray(data), self.labels.iloc[idx]['0']
+    
+class pzImg(Dataset):
+    """Custom dataset for spectrogram images"""
+    def __init__(self, label_file, data_dir, transform=None):
+        self.labels = pd.read_csv(label_file)
+        self.data_dir = data_dir
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        filename = self.labels.iloc[idx]['Filename']
+        filename = os.path.join(self.data_dir, (filename[:-4] + ".png"))
+        data = cv2.imread(filename) 
+        if self.transform:
+            data = self.transform(data)
+        return np.asarray(data), self.labels.iloc[idx]['0']
