efa39cf1af718796c489ebe75ecfef096178e203
AustinHCopley <austinhcopley@gmail.com>
2023-11-26 13:42:54 -0500

    started hyperparam grid search, introduced dropout

--- ezPzModels.py
+++ ezPzModels.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 
 class ezPzLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout=0.0):
         super(ezPzLSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout)
         self.fc = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
