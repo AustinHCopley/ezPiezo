3c1f47647572481abebf6b6bd01a7d9df2c75bc8
AustinHCopley <austinhcopley@gmail.com>
2024-01-21 14:57:20 -0500

    minor tweaks

--- ezPzModels.py
+++ ezPzModels.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.functional as F
 
 class ezPzLSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout=0.0):
